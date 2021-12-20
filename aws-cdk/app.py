1a67861e3a06cd01cf6ae80dbf93a9312727733f
Mike Connor <mike@sparkgeo.com>
2021-12-19 19:33:56 -0800

    Add Environment object.

--- aws-cdk/app.py
+++ aws-cdk/app.py
@@ -1,6 +1,6 @@
 import os
 
-from aws_cdk import App
+from aws_cdk import App, Environment
 
 from stacks.cicd_user import CICDStack
 from stacks.main import MainStack
@@ -11,15 +11,18 @@ tags = {
     "Environment": "sparkgeo-main",
     "Git Branch": "master",
     "Project": "PyPi",
-    "Repo": "https://github.com/sparkgeo/pypicloud-docker"
+    "Repo": "https://github.com/sparkgeo/pypicloud-docker",
 }
 
+env = Environment(
+    account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
+)
 
 app = App()
 
 # User for Github actions
-cicd_user = CICDStack(app, f"PyPiCICDUserStack", tags=tags)
+cicd_user = CICDStack(app, f"PyPiCICDUserStack", env=env, tags=tags)
 
-pypi = MainStack(app, f"PyPiMainStack", tags=tags)
+pypi = MainStack(app, f"PyPiMainStack", env=env, tags=tags)
 
 app.synth()
