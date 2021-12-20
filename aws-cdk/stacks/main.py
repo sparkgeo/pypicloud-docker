6635757403215843f8385f3412d7395a3432dad9
Mike Connor <mike@sparkgeo.com>
2021-12-19 20:15:39 -0800

    Comment out fargate to make vpc change.

--- aws-cdk/stacks/main.py
+++ aws-cdk/stacks/main.py
@@ -20,47 +20,47 @@ class MainStack(Stack):
 
         domain_name = "pypi.sparkge.dev"
 
-        pypi = patterns.ApplicationLoadBalancedFargateService(
-            self,
-            id="PyPiService",
-            platform_version=ecs.FargatePlatformVersion.VERSION1_4,
-            cluster=ecs.Cluster(
-                self,
-                f"Cluster",
-                vpc=vpc,
-                container_insights=True,
-            ),
-            cpu=2048,
-            memory_limit_mib=4096,
-            desired_count=1,
-            protocol=elb.ApplicationProtocol.HTTPS,
-            domain_zone=r53.HostedZone.from_lookup(
-                self, id="HostedZone", domain_name="sparkgeo.dev"
-            ),
-            domain_name=domain_name,
-            certificate=cert.Certificate(
-                self,
-                "PyPiCert",
-                domain_name=domain_name,
-                validation=cert.CertificateValidation.from_dns(),
-            ),
-            redirect_http=True,
-            task_image_options=patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_asset("../py3-baseimage"),
-                container_port=8080,
-                # environment={
-                #     "PGADMIN_DEFAULT_EMAIL": "dev@sparkgeo.com",
-                #     "PGADMIN_LISTEN_PORT": "5050",
-                # },
-                # secrets={
-                #     "PGADMIN_DEFAULT_PASSWORD": "",
-                # },
-                enable_logging=True,
-                # log_driver=ecs.LogDrivers.aws_logs(
-                #     stream_prefix=svc, log_group=log_group
-                # ),
-            ),
-        )
+        # pypi = patterns.ApplicationLoadBalancedFargateService(
+        #     self,
+        #     id="PyPiService",
+        #     platform_version=ecs.FargatePlatformVersion.VERSION1_4,
+        #     cluster=ecs.Cluster(
+        #         self,
+        #         f"Cluster",
+        #         vpc=vpc,
+        #         container_insights=True,
+        #     ),
+        #     cpu=2048,
+        #     memory_limit_mib=4096,
+        #     desired_count=1,
+        #     protocol=elb.ApplicationProtocol.HTTPS,
+        #     domain_zone=r53.HostedZone.from_lookup(
+        #         self, id="HostedZone", domain_name="sparkgeo.dev"
+        #     ),
+        #     domain_name=domain_name,
+        #     certificate=cert.Certificate(
+        #         self,
+        #         "PyPiCert",
+        #         domain_name=domain_name,
+        #         validation=cert.CertificateValidation.from_dns(),
+        #     ),
+        #     redirect_http=True,
+        #     task_image_options=patterns.ApplicationLoadBalancedTaskImageOptions(
+        #         image=ecs.ContainerImage.from_asset("../py3-baseimage"),
+        #         container_port=8080,
+        #         # environment={
+        #         #     "PGADMIN_DEFAULT_EMAIL": "dev@sparkgeo.com",
+        #         #     "PGADMIN_LISTEN_PORT": "5050",
+        #         # },
+        #         # secrets={
+        #         #     "PGADMIN_DEFAULT_PASSWORD": "",
+        #         # },
+        #         enable_logging=True,
+        #         # log_driver=ecs.LogDrivers.aws_logs(
+        #         #     stream_prefix=svc, log_group=log_group
+        #         # ),
+        #     ),
+        # )
 
         file_system = efs.FileSystem(
             self,
@@ -80,16 +80,16 @@ class MainStack(Stack):
             create_acl=efs.Acl(owner_gid="999", owner_uid="999", permissions="755"),
         )
 
-        pypi.service.task_definition.add_volume(
-            name="pypi-cloud",
-            efs_volume_configuration=ecs.EfsVolumeConfiguration(
-                file_system_id=file_system.file_system_id,
-                transit_encryption="ENABLED",
-                authorization_config=ecs.AuthorizationConfig(
-                    access_point_id=accessPoint.access_point_id, iam="ENABLED"
-                ),
-            ),
-        )
+        # pypi.service.task_definition.add_volume(
+        #     name="pypi-cloud",
+        #     efs_volume_configuration=ecs.EfsVolumeConfiguration(
+        #         file_system_id=file_system.file_system_id,
+        #         transit_encryption="ENABLED",
+        #         authorization_config=ecs.AuthorizationConfig(
+        #             access_point_id=accessPoint.access_point_id, iam="ENABLED"
+        #         ),
+        #     ),
+        # )
 
-        pypi.service.connections.allow_from(file_system, ec2.Port.tcp(2049))
-        pypi.service.connections.allow_to(file_system, ec2.Port.tcp(2049))
+        # pypi.service.connections.allow_from(file_system, ec2.Port.tcp(2049))
+        # pypi.service.connections.allow_to(file_system, ec2.Port.tcp(2049))
